08d526f74c4be61a5198c691dcca56544ee1653d
Farhan Uddin <farhanuddin2006@gmail.com>
2024-11-08 10:15:22 -0500

    Update neilnainani.java
    added main method

--- src/prj5/neilnainani.java
+++ src/prj5/neilnainani.java
@@ -4,7 +4,11 @@ public class neilnainani
     //~ Fields ................................................................
 
     //~ Constructors ..........................................................
-
+    public neilnainani() {
+        
+    }
     //~Public  Methods ........................................................
-
+    public static void main(String[] args) {
+        System.out.print("farhan was here");
+    }
 }
