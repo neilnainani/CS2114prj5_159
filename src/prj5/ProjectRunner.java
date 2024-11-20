022a65f03661c2577bd033221b81a52e7e64e736
cwalkva <cwalk@vt.edu>
2024-11-19 22:28:46 -0500

    Update ProjectRunner.java

--- src/prj5/ProjectRunner.java
+++ src/prj5/ProjectRunner.java
@@ -9,8 +9,43 @@ package prj5;
 
 public class ProjectRunner {
 
-    public ProjectRunner() {
-        // TODO Auto-generated constructor stub
+    public static void main(String[] args) throws IOException {
+        InputFileReader filer;
+        if (args.length > 0) {
+            filer = new InputFileReader(args[0]);
+        } else {
+            filer = new InputFileReader("SampleInput1_2023.csv");
+        }
+        
+        boolean showConsole = true;
+        boolean showGUI = false;
+        
+        InfluencerList influencerList = filer.readInputFile();
+        if (showConsole) {
+            DLinkedList<Influencer> combinedList = influencerList.getCombinedList();
+            Node<Influencer> currentNode = combinedList.getFirstNode();
+            while (currentNode != null) {
+                Influencer influencer = currentNode.getData();
+                System.out.println(influencer.getChannelName());
+                System.out.println("traditional: " + influencer.calculateTradEngagement());
+                System.out.println("==========");
+                currentNode = currentNode.getNext();
+            }
+            System.out.println("**********");
+            currentNode = combinedList.getFirstNode();
+            while (currentNode != null) {
+                Influencer influencer = currentNode.getData();
+                System.out.println(influencer.getChannelName());
+                System.out.println("reach: " + influencer.calculateReachEngagement());
+                System.out.println("==========");
+                currentNode = currentNode.getNext();
+            }
+        }
+        if (showGUI) {
+            /*
+            * TODO: For final Submission - Should have the GUI display for the input data
+            */
+        }
     }
 
 }
