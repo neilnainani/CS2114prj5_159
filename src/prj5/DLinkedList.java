f659abf0e375b896fffea5a997733b13428e28ad
Neil Nainani <112397496+neilnainani@users.noreply.github.com>
2024-11-21 14:38:56 -0500

    Node public and DLinkedList class and Test class

--- src/prj5/DLinkedList.java
+++ src/prj5/DLinkedList.java
@@ -3,64 +3,40 @@
 // As a Hokie, I will conduct myself with honor and integrity at all times.
 // I will not lie, cheat, or steal, nor will I 
 // accept the actions of those who do.
-// -- Noah Chen (noahc20)
+// -- Neil Nainani (neilnainani)
 
 package prj5;
 
+// -------------------------------------------------------------------------
+/**
+ *  This class implements a Doubly Linked List
+ *  @param <T>
+ * 
+ *  @author Neil Nainani (neilnainani)
+ *  @version Nov 19, 2024
+ */
 public class DLinkedList<T> {
-    
-    private static class Node<T> {
-        private Node<T> next;
-        private Node<T> prev;
-        private T data;
-        
-        
-        public Node(T d, Node<T> n, Node<T> p) {
-            next = n;
-            prev = p;
-            data = d;
-        }
-        
-        public Node(T d2) {
-            data = d2;
-        }
-        
-        public Node<T> getNext() {
-            return next;
-        }
-        
-        public Node<T> getPrev() {
-            return prev;
-        }
-        
-        public T getData() {
-            return data;
-        }
-        
-        public void setNext(Node<T> n) {
-            next = n;
-        }
-        
-        public void setData(T d) {
-            data = d;
-        }
-        
-        public void setPrev(Node<T> p) {
-            prev = p;
-        }
-    }
-    
+    /**
+     * firstNode is the head or the top Node in the list
+     * lastNode is the tail or the bottom Node in the list
+     * numberOfEntries is the number of entries in the list
+     */
     private Node<T> firstNode;
     private Node<T> lastNode;
     private int numberOfEntries;
-
+    
+    /**
+     * Creates a DLinkedList object while initializing a first and last Node
+     * @param f the front Node that gets initialized
+     * @param l the last Node that gets initialized
+     */
     public DLinkedList(Node<T> f, Node<T> l) {
         firstNode = f;
         lastNode = l;
-        if (firstNode != null && lastNode != null) {
+        if (firstNode.getData() != null && lastNode.getData() != null) {
             numberOfEntries = 2;
         }
-        else if(firstNode == null && lastNode == null) {
+        else if(firstNode.getData() == null && lastNode.getData() == null) {
             numberOfEntries = 0;
         }
         else {
@@ -68,52 +44,84 @@ public class DLinkedList<T> {
         }
     }
     
+    // ----------------------------------------------------------
+    /**
+     * Creates a DLinkedList object initializing the first and last Node to null
+     */
     public DLinkedList() {
         clear();
     }
     
+    // ----------------------------------------------------------
+
+    /**
+     * Adds a new Node to the top of the list
+     * @param data the data in the node to be added to the list
+     * @return boolean returns true at all times due to no restrictions on size
+     */
     public boolean add(T data) {
+        if (data == null) {
+            return false;
+        }
         Node<T> newNode = new Node<T>(data);
-        firstNode.setPrev(newNode);
-        firstNode = newNode;
+        if (firstNode != null) {
+            lastNode.setNext(newNode);
+            newNode.setPrev(lastNode);
+        }
+        else {
+            firstNode = newNode;
+        }
+        lastNode = newNode;
         numberOfEntries++;
         return true;
     }
     
+    /**
+     * Adds a new Node to a specific index in the list
+     * @param index the index where the Node will be added
+     * @param data the data in the Node to be added
+     * @return boolean returns true at all times due to no restrictions on size
+     */
     public boolean add(int index, T data) {
+        if (index > numberOfEntries || index < 0) {
+            return false;
+        }
+        if (numberOfEntries == 0) {
+            add(data);
+        }
         Node<T> newNode = new Node<T>(data);
         Node<T> currentNode = firstNode;
-        if (numberOfEntries - (index + 1) > (index + 1)) {
-            for (int i = 0; i < index - 1; i++) {
-                currentNode = currentNode.getNext();
-            }
-            currentNode.setNext(newNode);
+        for (int i = 0; i < index; i++) {
             currentNode = currentNode.getNext();
-            currentNode = currentNode.getNext();
-            currentNode.setPrev(newNode);
-            numberOfEntries++;
-            return true;
         }
-        currentNode = lastNode;
-        for (int i = numberOfEntries - 1; i > index + 1; i--) {
-            currentNode = currentNode.getPrev();
+        Node<T> n = new Node<T>(data, currentNode, currentNode.getPrev());
+        currentNode.getPrev().setNext(n);
+        currentNode.setPrev(n);
+        if (index == 0) {
+            firstNode = n;
         }
-        currentNode.setPrev(newNode);
-        currentNode = currentNode.getPrev();
-        currentNode = currentNode.getPrev();
-        currentNode.setNext(newNode);
+        if (index == numberOfEntries) {
+            lastNode = n;
+        }
         numberOfEntries++;
         return true;
     }
     
+    /**
+     * Clears the list of everything, acts a constructor with no parameters
+     */
     public void clear() {
-        firstNode = null;
-        lastNode = null;
+        firstNode = new Node<T>(null);
+        lastNode = new Node<T>(null);
         numberOfEntries = 0;
     }
-    
+    /**
+     * Checks if another object is equal to this one
+     * @param obj the object this is compared to
+     * @return boolean true if they equal each other, false otherwise
+     */
     public boolean equals(Object obj) {
-        if (obj == null || obj.getClass() != this.getClass()) {
+        if (obj == null || !(obj.getClass().equals(this.getClass()))) {
             return false;
         }
         if (obj == this) {
@@ -121,20 +129,29 @@ public class DLinkedList<T> {
         }
         @SuppressWarnings("unchecked")
         DLinkedList<T> o = (DLinkedList<T>)obj;
-        if (numberOfEntries != o.getNumberOfEntries() || firstNode.getData() !=
-            o.getFirstNode().getData() || lastNode.getData() != 
-            o.getLastNode().getData()) {
+        if (numberOfEntries != o.getNumberOfEntries() ||
+            !firstNode.getData().equals(o.getFirstNode().getData()) || 
+            !lastNode.getData().equals(o.getLastNode().getData()) ) 
+        {
             return false;
         }
         for (int i = 0; i < numberOfEntries; i++) {
-            if (!(this.get(i).equals(o.get(i)))) {
+            T tData = this.get(i);
+            T oData = o.get(i);
+            if (!(tData.equals(oData))) {
                 return false;
             }
         }
         return true;
     }
     
+    /**
+     * Returns the data in a node at a specific index
+     * @param i the index the node is retrieved from
+     * @return T the data retrieved, returns null if index isn't in range
+     */
     public T get(int i) {
+        /**
         if (i < 0 || i >= numberOfEntries) {
             return null;
         }
@@ -143,9 +160,21 @@ public class DLinkedList<T> {
             currNode = currNode.getNext();
         }
         return currNode.getData();
+        */
+        return getNode(i).getData();
     }
     
+    /**
+     * This is a helper method used to get a Node at a specific index
+     * @param i the specific index
+     * @return Node<T> the node at the specified index
+     * 
+     */
     private Node<T> getNode(int i) {
+        if (i < 0 || i >= numberOfEntries) {
+            Node<T> n = new Node<T>(null);
+            return n;
+        }
         Node<T> currNode = firstNode;
         for (int n = 0; n < i + 1; n++) {
             currNode = currNode.getNext();
@@ -153,6 +182,11 @@ public class DLinkedList<T> {
         return currNode;
     }
     
+    /**
+     * This method finds the index of a Node with the parameter's data in it
+     * @param d the data in which we find the index of
+     * @return int the index where the node with the paramater's data is at.
+     */
     public int indexOf(T d) {
         Node<T> currNode = firstNode;
         for (int i = 0; i < numberOfEntries; i++) {
@@ -164,6 +198,11 @@ public class DLinkedList<T> {
         return -1;
     }
     
+    /**
+     * This method removes a node at a specified index
+     * @param i the specified index
+     * @return boolean returns true if successful, false otherwise
+     */
     public boolean remove(int i) {
         if (i == 0) {
             firstNode.setData(null);
@@ -190,37 +229,74 @@ public class DLinkedList<T> {
         return true;
     }
     
+    /**
+     * Returns the number of entries in the list
+     * @return int the number of entries in the list
+     */
     public int getNumberOfEntries() {
         return numberOfEntries;
     }
     
+    /**
+     * Returns the first Node
+     * @return Node<T> the top Node
+     */
     public Node<T> getFirstNode() {
         return firstNode;
     }
     
+    /**
+     * Returns the last Node
+     * @return Node<T> the last Node
+     */
     public Node<T> getLastNode() {
         return lastNode;
     }
     
+    /**
+     * Adds a node to the front or top of the list
+     * @param n the node to be added
+     */
     public void addToFront(Node<T> n) {
         add(0, n.getData());
     }
+    
+    /**
+     * Adds a node to the back or bottom of the list
+     * @param n the node to be added
+     */
     public void addToBack(Node<T> n) {
         add(numberOfEntries, n.getData());
     }
     
+    /**
+     * Removes the top or front Node
+     */
     public void removeFront() {
         remove(0);
     }
     
+    /**
+     * Removes the back or bottom Node
+     */
     public void removeBack() {
         remove(numberOfEntries - 1);
     }
     
+    /**
+     * Checks if the list is empty or not
+     * @return boolean true if empty, false otherwise
+     */
     public boolean isEmpty() {
         return numberOfEntries == 0;
     }
     
+    // ----------------------------------------------------------
+    /**
+     * Checks if the list contains a certain data
+     * @param d the data being check if in the list or not
+     * @return boolean true if data is in list, false otherwise
+     */
     public boolean contains(T d) {
         Node<T> curr = firstNode;
         for (int i = 0; i < numberOfEntries; i++) {
---
+++ src/prj5/DLinkedListTest.java
@@ -0,0 +1,113 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I 
+// accept the actions of those who do.
+// -- Neil Nainani (neilnainani)
+package prj5;
+
+import student.TestCase;
+
+// -------------------------------------------------------------------------
+/**
+ *  This class tests all the methods in DLinkedList
+ * 
+ *  @author Neil Nainani (neilnainani)
+ *  @version Nov 20, 2024
+ */
+public class DLinkedListTest extends TestCase
+{
+    //~ Fields ................................................................
+    /**
+     * emptyA an empty DLinkedList used for testing
+     * emptyB an empty DLinkedList used for testing
+     * smallA a small DLinkedList used for testing
+     * smallB a small DLinkedList used for testing
+     * bigA a big DLinkedlist used for testing
+     * bigB a big DLinkedList used for testing
+     * nullString a String that is null that's used for testing
+     */
+    
+    private DLinkedList<String> emptyA;
+    private DLinkedList<String> emptyB;
+    private DLinkedList<String> smallA;
+    private DLinkedList<String> smallB;
+    private DLinkedList<String> bigA;
+    private DLinkedList<String> bigB;
+    private String nullString;
+
+
+    /**
+     * Initializes 2 empty lists, 2 lists with a small number of items, and 2
+     * lists with a large number of items
+     */
+    public void setUp() {
+        emptyA = new DLinkedList<String>();
+        emptyB = new DLinkedList<String>();
+
+        smallA = new DLinkedList<String>();
+        smallB = new DLinkedList<String>();
+
+        smallA.add("soccer");
+        smallA.add("swimming");
+        smallA.add("gymnastics");
+
+        smallB.add("soccer");
+        smallB.add("swimming");
+        smallB.add("gymnastics");
+
+        bigA = new DLinkedList<String>();
+
+        for (int i = 0; i < 100; i++) {
+            bigA.add("sport" + i);
+        }
+
+        bigB = new DLinkedList<String>();
+        for (int i = 0; i < 100; i++) {
+            bigB.add("sport" + i);
+        }
+        
+        // to be explicit
+        nullString = null;
+    }
+    //~ Constructors ..........................................................
+    
+    //~Public  Methods ........................................................
+    // ----------------------------------------------------------
+    /**
+     * This method tests the add() method
+     */
+    public void testAdd() {
+        emptyA.add("q");
+        assertEquals(emptyA.getFirstNode().getData(), "q");
+        assertEquals(emptyA.getLastNode().getData(), "q");
+        assertEquals(emptyA.getNumberOfEntries(), 1);
+        emptyA.add("w");
+        emptyA.add(1, "e");
+        assertEquals(emptyA.getFirstNode().getData(), "w");
+        assertEquals(emptyA.getLastNode().getData(), "q");
+        assertEquals(emptyA.getNumberOfEntries(), 3);
+        assertEquals(emptyA.getFirstNode().getNext().getData(), "e");
+        assertEquals(emptyA.add(5, "false"), false);
+        assertEquals(emptyA.add(-1, "false"), false);
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * This method tests the clear() method
+     */
+    public void testClear() {
+        bigB.clear();
+        assertEquals(0, bigB.getNumberOfEntries());
+        assertEquals(null, bigB.getFirstNode().getData());
+        assertEquals(null, bigB.getLastNode().getData());
+    }
+    
+    public void testEquals() {
+        assertEquals(true, smallA.equals(smallB));
+        assertEquals(false, smallA.equals(nullString));
+        int i = 0;
+        assertEquals(false, smallA.equals(i));
+        assertEquals(true, smallB.equals(smallB));
+    }
+}
