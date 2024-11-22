55718d9644dda80842a82b65fc2706ec2b201457
cwalkva <cwalk@vt.edu>
2024-11-21 19:49:59 -0500

    Update InfluencerListCalculatorTest.java

--- src/prj5/InfluencerListCalculatorTest.java
+++ src/prj5/InfluencerListCalculatorTest.java
@@ -1,76 +1,159 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I
-// accept the actions of those who do.
-// -- Chloe Walker (cwalk)
-
 package prj5;
 
 /**
  * Test class for InfluencerListCalculator
  * 
- * @author chloe
+ * @author Chloe
  * @version Nov 19, 2024
  */
-public class InfluencerListCalculatorTest extends student.TestCase {
+public class InfluencerListCalculatorTest
+    extends student.TestCase
+{
     private InfluencerListCalculator calc;
-    private InfluencerList inflList;
+    private DLinkedList<Influencer> influencerList;
 
     /**
      * Sets up the InfluencerList and InfluencerListCalculator objects needed
      * for the test class.
      */
-    public void setUp() {
-        DLinkedList<Influencer> influencerList = new DLinkedList<Influencer>();
+    public void setUp()
+    {
+        influencerList = new DLinkedList<>();
         influencerList.add(
-            new Influencer("January", "UserA", "abc", 100, 2000, 300, 4000));
-        // 20 10
+            new Influencer("January", "UserA", "abc", 100, 2000, 300, 4000)); // 20
+                                                                              // 10
         influencerList.add(
-            new Influencer("January", "UserB", "ghi", 700, 400, 390, 5000));
-        // 298 21
+            new Influencer("January", "UserB", "ghi", 700, 400, 390, 5000)); // 298
+                                                                             // 21
         influencerList.add(
-            new Influencer("January", "UserC", "jki", 200, 800, 700, 90000));
-        // 111 1
+            new Influencer("January", "UserC", "jki", 200, 800, 700, 90000)); // 111
+                                                                              // 1
         influencerList.add(
-            new Influencer("January", "UserD", "def", 400, 300, 500, 2000));
-        // 300 45
+            new Influencer("January", "UserD", "def", 400, 300, 500, 2000)); // 300
+                                                                             // 45
 
         calc = new InfluencerListCalculator(influencerList);
-        inflList = new InfluencerList(influencerList,
-            new DLinkedList<Influencer>(), new DLinkedList<Influencer>());
     }
 
-    /**
-     * tests the sortName() method
-     */
-    public void testSortName() {
-        calc.sortName();
-        assertEquals("abc", inflList.getOneList().get(0).getChannelName());
-        assertEquals("def", inflList.getOneList().get(1).getChannelName());
-        assertEquals("ghi", inflList.getOneList().get(2).getChannelName());
-        assertEquals("jki", inflList.getOneList().get(3).getChannelName());
-    }
 
     /**
-     * tests the sortTradER() method
+     * Tests the sortName() method.
      */
-    public void testSortTradER() {
-        calc.sortTradER();
-        assertEquals("def", inflList.getOneList().get(0).getChannelName());
-        assertEquals("ghi", inflList.getOneList().get(1).getChannelName());
-        assertEquals("jki", inflList.getOneList().get(2).getChannelName());
-        assertEquals("abc", inflList.getOneList().get(3).getChannelName());
+    public void testSortName()
+    {
+        DLinkedList<Influencer> sorted = calc.sortName();
+        assertEquals("abc", sorted.get(0).getChannelName());
+        assertEquals("def", sorted.get(1).getChannelName());
+        assertEquals("ghi", sorted.get(2).getChannelName());
+        assertEquals("jki", sorted.get(3).getChannelName());
     }
 
+
     /**
-     * tests the sortReachER() method
+     * Tests the sortTradER() method.
      */
-    public void testSortReachER() {
-        calc.sortReachER();
-        assertEquals("def", inflList.getOneList().get(0).getChannelName());
-        assertEquals("ghi", inflList.getOneList().get(1).getChannelName());
-        assertEquals("abc", inflList.getOneList().get(2).getChannelName());
-        assertEquals("jki", inflList.getOneList().get(3).getChannelName());
+    public void testSortTradER()
+    {
+        DLinkedList<Influencer> sorted = calc.sortTradER();
+        assertEquals("def", sorted.get(0).getChannelName());
+        assertEquals("ghi", sorted.get(1).getChannelName());
+        assertEquals("jki", sorted.get(2).getChannelName());
+        assertEquals("abc", sorted.get(3).getChannelName());
+    }
+
+
+    /**
+     * Tests the sortReachER() method.
+     */
+    public void testSortReachER()
+    {
+        DLinkedList<Influencer> sorted = calc.sortReachER();
+        assertEquals("def", sorted.get(0).getChannelName());
+        assertEquals("ghi", sorted.get(1).getChannelName());
+        assertEquals("abc", sorted.get(2).getChannelName());
+        assertEquals("jki", sorted.get(3).getChannelName());
+    }
+
+
+    /**
+     * Tests sorting an empty list.
+     */
+    public void testSortEmptyList()
+    {
+        DLinkedList<Influencer> emptyList = new DLinkedList<>();
+        calc = new InfluencerListCalculator(emptyList);
+        DLinkedList<Influencer> sortedList = calc.sortName();
+        assertTrue(sortedList.isEmpty());
+    }
+
+
+    /**
+     * Tests sorting a list with only one entry
+     */
+    public void testSortSingleEntryList()
+    {
+        DLinkedList<Influencer> singleList = new DLinkedList<>();
+        singleList.add(
+            new Influencer("January", "UserA", "abc", 100, 2000, 300, 4000));
+        calc = new InfluencerListCalculator(singleList);
+        DLinkedList<Influencer> sorted = calc.sortName();
+        assertEquals(1, sorted.getNumberOfEntries());
+        assertEquals("abc", sorted.get(0).getChannelName());
+    }
+
+
+    /**
+     * Tests sorting influencers with the same reach engagement rate.
+     */
+    public void testSameReach()
+    {
+        influencerList.add(
+            new Influencer("January", "UserE", "klm", 500, 400, 500, 2000));
+        calc = new InfluencerListCalculator(influencerList);
+        DLinkedList<Influencer> sorted = calc.sortReachER();
+        assertEquals("klm", sorted.get(0).getChannelName());
+        assertEquals("def", sorted.get(1).getChannelName());
+    }
+
+
+    /**
+     * Tests sorting influencers with the same traditional engagement rate.
+     */
+    public void testSameTrad()
+    {
+        influencerList.add(
+            new Influencer("January", "UserE", "klm", 200, 300, 500, 2000));
+        calc = new InfluencerListCalculator(influencerList);
+        DLinkedList<Influencer> sorted = calc.sortTradER();
+        assertEquals("def", sorted.get(0).getChannelName());
+        assertEquals("klm", sorted.get(2).getChannelName());
+    }
+
+
+    /**
+     * Tests influencers with the same channel name.
+     */
+    public void testSameName()
+    {
+        influencerList.add(
+            new Influencer("January", "UserE", "ghi", 700, 400, 390, 5000));
+        calc = new InfluencerListCalculator(influencerList);
+        DLinkedList<Influencer> sorted = calc.sortName();
+        assertEquals("ghi", sorted.get(2).getChannelName());
+        assertEquals("ghi", sorted.get(3).getChannelName());
+    }
+
+
+    /**
+     * Tests influencers with similar channel names.
+     */
+    public void testSimilarName()
+    {
+        influencerList.add(
+            new Influencer("January", "UserE", "ghiA", 700, 400, 390, 5000));
+        calc = new InfluencerListCalculator(influencerList);
+        DLinkedList<Influencer> sorted = calc.sortName();
+        assertEquals("ghi", sorted.get(2).getChannelName());
+        assertEquals("ghiA", sorted.get(3).getChannelName());
     }
 }
