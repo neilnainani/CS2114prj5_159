857aff1cedd8cfb5d94c3a7fed43b814100712f4
NoahChen2 <96897160+NoahChen2@users.noreply.github.com>
2024-12-03 18:22:31 -0500

    Update GUIProjectWindow.java

--- src/prj5/GUIProjectWindow.java
+++ src/prj5/GUIProjectWindow.java
@@ -1,4 +1,4 @@
-// Virginia Tech Honor Code Pledge:
+// Virginia Tech Honor Code Pledge = new
 //
 // As a Hokie, I will conduct myself with honor and integrity at all times.
 // I will not lie, cheat, or steal, nor will I 
@@ -7,10 +7,151 @@
 
 package prj5;
 
-public class GUIProjectWindow {
+import java.awt.Color;
 
-    public GUIProjectWindow() {
-        // TODO Auto-generated constructor stub
+import cs2.Button;
+import cs2.Shape;
+import cs2.Window;
+import cs2.WindowSide;
+
+/**
+ * Displays the window
+ * 
+ * @author Noah Chen (noahc20)
+ * @version 2024.12.03
+ */
+public class GUIProjectWindow {
+    private InfluencerList influencerRankinds;
+    private Window window;
+    private Shape[] graphShapes;
+    private static Color[] graphColors = new Color[8];
+
+    private static final double DISPLAY_FACTOR = 1.5;
+    private int graph_x;
+    private int graph_y;
+    private static final int BAR_WIDTH = 30;
+
+    private Button sortByChannel;
+    private Button sortByEngagement;
+    private Button quit;
+    private Button tradEngagement;
+    private Button reachEngagement;
+    private Button january;
+    private Button february;
+    private Button march;
+    private Button firstQuarter;
+
+    /**
+     * Constructor that creates the window and buttons
+     * 
+     * @param iFL an InfluencerList that contains 3 different months of
+     *            influencers
+     */
+    public GUIProjectWindow(InfluencerList iFL) {
+        window = new Window("Social Media Vis");
+        window.setSize((int) (500 * DISPLAY_FACTOR),
+            (int) (500 * DISPLAY_FACTOR));
+
+        influencerRankinds = iFL;
+        sortByChannel = new Button("Sort by Channel Name");
+        window.addButton(sortByChannel, WindowSide.NORTH);
+        sortByChannel.onClick(this, "clickedSortChannel");
+
+        sortByEngagement = new Button("Sort by Engagement Rate");
+        window.addButton(sortByEngagement, WindowSide.NORTH);
+        sortByEngagement.onClick(this, "clickedSortEngage");
+
+        quit = new Button("Quit");
+        window.addButton(quit, WindowSide.NORTH);
+        sortByEngagement.onClick(this, "clickedQuit");
+
+        tradEngagement = new Button("Traditional Engagement Rate");
+        window.addButton(tradEngagement, WindowSide.NORTH);
+        tradEngagement.onClick(this, "clickedTradEngagement");
+
+        reachEngagement = new Button("Reach Engagement Rate");
+        window.addButton(reachEngagement, WindowSide.NORTH);
+        reachEngagement.onClick(this, "clickedReachEngagement");
+
+        january = new Button("January");
+        window.addButton(january, WindowSide.NORTH);
+        january.onClick(this, "clickedJanuary");
+
+        february = new Button("February");
+        window.addButton(february, WindowSide.NORTH);
+        february.onClick(this, "clickedFebruary");
+
+        march = new Button("March");
+        window.addButton(march, WindowSide.NORTH);
+        march.onClick(this, "clickedMarch");
+
+        firstQuarter = new Button("First Quarter (Jan - March");
+        window.addButton(firstQuarter, WindowSide.NORTH);
+        firstQuarter.onClick(this, "clickedFirstQuarter");
+
+        graphColors[0] = new Color(51, 92, 103); // blue
+        graphColors[1] = new Color(224, 159, 62); // yellow
+        graphColors[2] = new Color(158, 42, 43); // red
+        graphColors[3] = new Color(17, 17, 17); // black
+        graphColors[4] = new Color(38, 68, 77); // dark blue
+        graphColors[5] = new Color(97, 69, 27); // dark yellow
+        graphColors[6] = new Color(71, 19, 19); // dark red
+        graphColors[7] = new Color(255, 255, 255); // white
+
+        updateText();
+        updateGraph();
+        drawShapes();
+    }
+
+    private void drawShapes() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void updateGraph() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void updateText() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void clickedSortChannel() {
+
+    }
+
+    private void clickedSortEngage() {
+
+    }
+
+    private void clickedQuit() {
+
+    }
+
+    private void clickedTradEngagement() {
+
+    }
+
+    private void clickedReachEngagement() {
+
+    }
+
+    private void clickedJanuary() {
+
+    }
+
+    private void clickedFebruary() {
+
+    }
+
+    private void clickedMarch() {
+
+    }
+
+    private void clickedFirstQuarter() {
+
     }
 
 }
